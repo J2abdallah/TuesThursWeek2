be7529761d6b7f4538ed0c9bf47f2c683bdcd001
j2abdallah <j2abdallah@student.bridgew.edu>
2021-09-07 10:17:42 -0400

    dogyear

--- src/bsu/comp152/Main.java
+++ src/bsu/comp152/Main.java
@@ -1,8 +1,16 @@
 package bsu.comp152;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var reader = new Scanner(System.in);
+        System.out.println("How old are you? ");
+        var age = reader.nextInt();
+        var dogYears = age*7;
+        System.out.println("Wow " + age + " years old!");
+
+        System.out.println("That's " + dogYears + " in dog years.");
     }
 }
